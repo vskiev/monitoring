aafd433d6d04771bd1ce2265cde06587aad3e444
vlad <vskiev@gmail.com>
2016-06-07 08:52:07 +0300

    aded

--- src/com/google/vskiev/mainpakage/MainServlet.java
+++ src/com/google/vskiev/mainpakage/MainServlet.java
@@ -1,7 +1,29 @@
 package com.google.vskiev.mainpakage;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by vlad on 07.06.16.
  */
-public class MainServlet {
+public class MainServlet extends HttpServlet
+{
+ public MainServlet()
+ {
+     super();
+ }
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
